36f278ce7cf0d3bb10bb76c6773ad3e89d67fcc2
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-13 16:04:13 +0530

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import Nav from './components/Nav';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path='/' element={<h1>Product Listing Component</h1>}></Route>
+        <Route path='/add' element={<h1>Add Product Component</h1>}></Route>
+        <Route path='/update' element={<h1>Update Product Component</h1>}></Route>
+        <Route path='/logout' element={<h1>Logout</h1>}></Route>
+        <Route path='/profile' element={<h1>Profile Component</h1>}></Route>
+      </Routes>
+      <Footer />
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
