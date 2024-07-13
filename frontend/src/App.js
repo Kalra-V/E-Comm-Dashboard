d9d84189c6d588dc5087d042f79740a69bf0bf1d
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-13 16:39:02 +0530

    #6 - SignUp page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Nav from './components/Nav';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/update' element={<h1>Update Product Component</h1>}></Route>
         <Route path='/logout' element={<h1>Logout</h1>}></Route>
         <Route path='/profile' element={<h1>Profile Component</h1>}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
       </Routes>
       <Footer />
     </div>
