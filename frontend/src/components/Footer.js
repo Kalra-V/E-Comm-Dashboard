4c7c447bed4f3aa9076dbd2600ce9b4141e85c5b
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-13 16:04:52 +0530

    Added Header and Footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="footer">
-            <h3>E-Comm Dashboard Footer LETS SEE</h3>
+            <h3>E-Comm Dashboard Footer</h3>
         </div>
     )
 }
