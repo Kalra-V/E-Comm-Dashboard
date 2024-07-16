e97871502b779752b4e6615989bf5a8ace19263d
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-16 22:35:11 +0530

    Completed signup flow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Signup from './components/Signup';
+import PrivateComponent from './components/PrivateComponent';
 
 function App() {
   return (
@@ -10,11 +11,15 @@ function App() {
     <div className="App">
       <Nav />
       <Routes>
+
+        <Route element={<PrivateComponent />}>
         <Route path='/' element={<h1>Product Listing Component</h1>}></Route>
         <Route path='/add' element={<h1>Add Product Component</h1>}></Route>
         <Route path='/update' element={<h1>Update Product Component</h1>}></Route>
         <Route path='/logout' element={<h1>Logout</h1>}></Route>
         <Route path='/profile' element={<h1>Profile Component</h1>}></Route>
+        </Route>
+        
         <Route path='/signup' element={<Signup />}></Route>
       </Routes>
       <Footer />
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,26 +1,34 @@
-import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+
   const navigate = useNavigate();
-  const collectData = async () => {
-    console.log(name, email, password);
-    let result = await fetch('http://localhost:5000/register', {
-        method: 'post',
-        body: JSON.stringify({name,email,password}),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-    });
-    result = await result.json()
-    console.log(result)
-    if(result) {
-        navigate('/')
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
     }
-  }
+  });
+
+  const collectData = async () => {
+    let result = await fetch("http://localhost:5000/register", {
+      method: "post",
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.log(result);
+    localStorage.setItem("user", JSON.stringify(result));
+    if (result) {
+      navigate("/");
+    }
+  };
   return (
     <div className="register">
       <h1>Register</h1>
