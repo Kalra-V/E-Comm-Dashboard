c7b3ad04b2533ce2425e22b51f8748f0b195e558
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-17 22:21:33 +0530

    Login API integration with frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {useNavigate} from 'react-router-dom'
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleLogin = () => {
-    console.log(email, password)
+  const navigate = useNavigate();
+  useEffect(()=> {
+    const auth = localStorage.getItem('user');
+    if(auth) {
+      navigate("/");
+    }
+  }, [])
+  const handleLogin = async () => {
+    console.log(email, password);
+    let result = await fetch("http://localhost:5000/login", {
+        method:'post',
+        body: JSON.stringify({email,password}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    })
+    result = await result.json();
+    if(result.name) {
+      localStorage.setItem("user", JSON.stringify(result));
+      navigate('/')
+      
+    } else {
+      alert("Wrong login credentials.")
+    }
+    console.log(result)
   }
   return (
     <div className="login">
+      <h1>Login</h1>
       <input
         type="text"
         placeholder="Enter Email"
